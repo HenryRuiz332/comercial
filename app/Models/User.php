8e1fdb64945a8e528436a2a44b3e635bc99f9028
Henry <henryruiz332@gmail.com>
2021-06-18 17:17:24 -0400

    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,5 +1,55 @@
 <?php
-	$path = $_SERVER['DOCUMENT_ROOT'];
-	$path = explode("public", $path);
-	$path = $path[0]; 
-    require_once ($path. "App/Models/Users/User.php");
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Servicios\ClienteServicio;
+
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable, Softdeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nombre',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+
+    public function clienteServicio(){
+        return $this->hasMany(ClienteServicio::class, 'user_id');
+    } 
+
+}
+
+
